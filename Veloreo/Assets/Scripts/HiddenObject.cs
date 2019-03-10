a864218ee019b527877a153db57286354949b21d
Temi Taylor <temit@mit.edu>
2019-03-10 19:46:54 -0400

    Isolating code responsible for hiding objects

---
+++ Veloreo/Assets/Scripts/HiddenObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains state for objects that are meant to be hidden by the darkness, and allows
+/// their visibility to be toggled.
+/// </summary>
+public class HiddenObject : MonoBehaviour
+{
+    public bool debugVisibility = false;
+    public Color normalColor;
+    public Color visibleColor;
+
+    private SpriteRenderer rend;
+
+    public delegate void VisibilityAction(bool visible);
+    private static VisibilityAction setAllVisible;
+
+    //// Static Methods
+
+    /// <summary>
+    /// Change the visibility of all active objects.
+    /// </summary>
+    /// <param name="visible">Whether or not objects should be visible on a black background</param>
+    public static void setVisibility(bool visible)
+    {
+        setAllVisible?.Invoke(visible); //calls if it is not null
+    }
+
+    //// Private Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        setLocalVisibility(debugVisibility);
+    }
+
+    /// <summary>
+    /// Sets the visibility of an individual object.
+    /// </summary>
+    /// <param name="visible">Whether or not the object should be visible on a black background</param>
+    private void setLocalVisibility(bool visible)
+    {
+        rend.color = visible ? visibleColor : normalColor;
+    }
+
+    // Add visibility method to static delegate
+    private void OnEnable()
+    {
+        HiddenObject.setAllVisible += setLocalVisibility;
+    }
+    private void OnDisable()
+    {
+        HiddenObject.setAllVisible -= setLocalVisibility;
+    }
+}
--- Veloreo/Assets/Scripts/Wall.cs
+++ Veloreo/Assets/Scripts/Wall.cs
@@ -3,31 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Contains some information about a wall's state, as well as options for debugging
-/// like visibility.
+/// Contains some information about a wall's state.
 /// </summary>
 public class Wall : MonoBehaviour
 {
-    public bool debugVisibility = false;
-    public Color normalColor;
-    public Color visibleColor;
-
-    private SpriteRenderer rend;
-
-    public delegate void VisibilityAction(bool visible);
-    private static VisibilityAction setAllVisible;
-
-    //// Static Methods
-
-    /// <summary>
-    /// Change the visibility of all active wall objects.
-    /// </summary>
-    /// <param name="visible">Whether or not walls should be visible on a black background</param>
-    public static void setVisibility(bool visible)
-    {
-        setAllVisible?.Invoke(visible); //calls if it is not null
-    }
-
     //// Observers
     
     public enum Orientation { horizontal, vertical, square };
@@ -51,33 +30,5 @@ public class Wall : MonoBehaviour
         {
             return xScale > yScale ? Orientation.horizontal : Orientation.vertical;
         }
-    }
-
-    //// Private Methods
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rend = GetComponent<SpriteRenderer>();
-        setLocalVisibility(debugVisibility);
-    }
-
-    /// <summary>
-    /// Sets the visibility of an individual wall object.
-    /// </summary>
-    /// <param name="visible">Whether or not the wall should be visible on a black background</param>
-    private void setLocalVisibility(bool visible)
-    {
-        rend.color = visible ? visibleColor : normalColor;
-    }
-
-    // Add visibility method to static delegate
-    private void OnEnable()
-    {
-        Wall.setAllVisible += setLocalVisibility;
-    }
-    private void OnDisable()
-    {
-        Wall.setAllVisible -= setLocalVisibility;
     }
 }
