47a4957f28acf303e85a8102653f0494e392e7c8
Emily Mattlin <emilyjmattlin@gmail.com>
2019-03-13 13:14:15 -0400

    Health display
    Controls health prefab and health bar

---
+++ Veloreo/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains actions that control the display of the health bar UI
+/// </summary>
+public class HealthDisplay : MonoBehaviour
+{   
+    //refers to health cubes in UI
+    public string[] healthTags = { "health0", "health1", "health2", "health3", "health4" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Takes one health/life away from health bar display when called--called by PlayerData in doDamage function
+    /// </summary>
+    public void changeHealthDisplay()
+    {
+        if(gameObject.tag == healthTags[FindObjectOfType<PlayerData>().getHealth()])
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
