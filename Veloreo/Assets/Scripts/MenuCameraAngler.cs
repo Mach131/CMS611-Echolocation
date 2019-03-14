d8f936bfe820cbc14cbe81b6ea0eda288f5c8a6b
Pablo Villalobos <pvillalo@mit.edu>
2019-03-13 20:41:57 -0400

    Add sample menu scenes

---
+++ Veloreo/Assets/Scripts/MenuCameraAngler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraAngler : MonoBehaviour
+{
+    public int i = 0;
+    public List<Vector3> positions = new List<Vector3>();
+    public List<Vector3> rotations = new List<Vector3>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = positions[i];
+        transform.SetPositionAndRotation(positions[i], Quaternion.Euler(rotations[i]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Veloreo/Assets/Scripts/MenuFloorAnimator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFloorAnimator : MonoBehaviour
+{
+    public GameObject floorTile;
+    public Material mat;
+    public int boardSize;
+    public float wavePd;
+
+    private List<GameObject> floorTiles = new List<GameObject>();
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildFloor(boardSize);
+    }
+
+    /// <summary>
+    /// Builds a game board of floor tiles
+    /// </summary>
+    /// <param name="size">The size of the game board</param>
+    void BuildFloor(int size)
+    {
+        for (int i = -size; i <= size; i++)
+        {
+            for (int j = -size; j <= size; j++)
+            {
+                Vector3 position = new Vector3(i * .5f, 0f, j * .5f);
+                GameObject tile = Instantiate(floorTile, position, Quaternion.identity);
+                tile.name = "Tile " + i + ", " + j;
+                tile.transform.parent = transform;
+                tile.GetComponent<MeshRenderer>().material = mat;
+                floorTiles.Add(tile);
+            }
+        }
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        foreach (GameObject pt in floorTiles)
+        {
+            float x = pt.transform.position.x;
+            float z = pt.transform.position.z;
+            pt.transform.position = new Vector3(x,
+                                                f(x, z, time),
+                                                z);
+        }
+    }
+
+    float f(float x, float y, float t)
+    {
+        //return Mathf.Sin(2 * x + t) / 5;
+        //return Mathf.Sin(2*x + t) + Mathf.Cos(2*y + t);
+        return Mathf.Sin((x * x + y * y) * wavePd - t) / 6f;
+    }
+}
