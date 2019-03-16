6086d5dd43f29c0f1beb644da65df2899dde2460
Emily Mattlin <emilyjmattlin@gmail.com>
2019-03-16 14:15:36 -0400

    waves remaining text display

---
+++ Veloreo/Assets/Scripts/WaveText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Controls text for the waves remaining display
+/// </summary>
+public class WaveText : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+    string startingWaveAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingWaveAmount = (FloorManager.maximumWaves).ToString();
+        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.text = startingWaveAmount + " waves remaining";
+    }
+
+    /// <summary>
+    /// Changes the display of the waves remaining text to update to the
+    /// current amount of remaining waves
+    /// </summary>
+    /// /// <param name="wavesRemaining">The amount of waves left</param>
+    public void changeTextDisplay(int wavesRemaining)
+    {
+        textMesh.text = wavesRemaining.ToString() + " waves remaining";
+    }
+
+    void Update()
+    {
+
+    }
+}
