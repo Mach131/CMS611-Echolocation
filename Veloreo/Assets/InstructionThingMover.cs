dc346e91934b708cc952b42a4645f8e7b0dbec89
Pablo Villalobos <pvillalo@mit.edu>
2019-03-19 12:35:04 -0400

    fix instructions gg

---
+++ Veloreo/Assets/InstructionThingMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionThingMover : MonoBehaviour
+{
+    public Camera cam;
+
+    public float xPct;
+    public float yPct;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(xPct*Screen.width, yPct*Screen.height, .5f));
+    }
+}
