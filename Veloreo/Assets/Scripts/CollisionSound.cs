998704d586d1db49e803bec76b56cc1313c02e82
Emily Mattlin <emilyjmattlin@gmail.com>
2019-03-20 00:11:15 -0400

    sounds for collision

---
+++ Veloreo/Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    private AudioSource note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        note = GetComponent<AudioSource>();        
+    }
+    
+    public void collideSound()
+    {
+        note.Play(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
